3052892996f76ac589b1a046eac9f4057716c8ff
cebuka1 <ebusco29@gmail.com>
2024-05-08 16:56:07 +0100

    project

---
+++ src/main/java/class_activity_5A/SimpleCalculator.java
@@ -0,0 +1,10 @@
+package class_activity_5A;
+
+public class SimpleCalculator {
+
+    public int addTwoNumbers(int num1, int num2){
+
+        return num1 + num2;
+
+    }
+}
---
+++ src/test/java/class_activity_5A/SimpleCalculatorTest.java
@@ -0,0 +1,18 @@
+package class_activity_5A;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleCalculatorTest {
+
+    @Test
+    void itShouldAddTwoNumbers() {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        int actual = calculator.addTwoNumbers(2,2);
+        int expected = 4;
+
+        assertEquals(4, actual);
+    }
+}
